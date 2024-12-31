02e269d61d9e0d637eb4053eae04a2ef166ce02d
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2024-12-31 22:06:52 +0300

    Develop home.jsx component for main page layout  - Created the main structure for the homepage with header, banner, and category sections - Integrated dynamic content rendering for category items - Added responsive design to ensure proper layout on different screen sizes - Implemented basic interactivity with buttons and navigation links
    Develop home.jsx component for main page layout
    
    - Created the main structure for the homepage with header, banner, and category sections
    - Integrated dynamic content rendering for category items
    - Added responsive design to ensure proper layout on different screen sizes
    - Implemented basic interactivity with buttons and navigation links

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+
+import Content from "../components/Content";
+import Footers from "../components/Footer";
+import Head from "../components/Head";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+
+export default function Home () {
+  
+
+    return(
+        <>
+        <Head/>
+       <Header/>
+       <Navbar/>
+       <Content/>
+      <Footers/>
+        </>
+    )
+}
