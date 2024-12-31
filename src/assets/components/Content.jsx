2e64e47f591aac3e9f6bbda7eff058e3d97cd730
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2024-12-31 04:44:22 +0300

    content imports

---
+++ src/assets/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+import ramenIcon from "/images/iteration-2-images/icons/1.svg";
+import pizzaIcon from "/images/iteration-2-images/icons/2.svg";
+import burgerIcon from "/images/iteration-2-images/icons/3.svg";
+import friesIcon from "/images/iteration-2-images/icons/4.svg";
+import fastFoodIcon from "/images/iteration-2-images/icons/5.svg";
+import drinksIcon from "/images/iteration-2-images/icons/6.svg";
+import food1 from "/images/iteration-2-images/pictures/food-1.png";
+import food2 from "/images/iteration-2-images/pictures/food-2.png";
+import food3 from "/images/iteration-2-images/pictures/food-3.png";
