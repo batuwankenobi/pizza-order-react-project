c62c738605e2704bad9807ddc821417a954dbf9e
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2024-12-31 04:47:37 +0300

    imports for footer section

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import logoFooter from "/images/iteration-2-images/footer/logo-footer.svg";
+import icon1 from "/images/iteration-2-images/footer/icons/icon-1.png";
+import icon2 from "/images/iteration-2-images/footer/icons/icon-2.png";
+import icon3 from "/images/iteration-2-images/footer/icons/icon-3.png";
+import insta1 from "/images/iteration-2-images/footer/insta/li-0.png";
+import insta2 from "/images/iteration-2-images/footer/insta/li-1.png";
+import insta3 from "/images/iteration-2-images/footer/insta/li-2.png";
+import insta4 from "/images/iteration-2-images/footer/insta/li-3.png";
+import insta5 from "/images/iteration-2-images/footer/insta/li-4.png";
+import insta6 from "/images/iteration-2-images/footer/insta/li-5.png";
