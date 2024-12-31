2dedaf953846ee246ca7eacc9dd72670a316fd38
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2024-12-31 05:04:31 +0300

    Add Home component with Head, Header, Navbar, Content, and Footer
    Add Home component with Head, Header, Navbar, Content, and Footer

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -4,3 +4,17 @@ import Footers from "../components/Footer";
 import Head from "../components/Head";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+
+const Home = () => {
+	return (
+	  <>
+	    <Head />
+	    <Header />
+	    <Navbar />
+	    <Content />
+	    <Footers />
+	  </>
+	);
+    };
+    
+    export default Home;
