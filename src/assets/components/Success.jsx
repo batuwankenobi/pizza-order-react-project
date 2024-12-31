0d36339ecba36a9424ba2cb42678515acbc10f6f
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2024-12-31 04:55:27 +0300

    css
    sipariş verince cıkan ekran için css

---
+++ src/assets/components/Success.jsx
@@ -0,0 +1,14 @@
+/* Genel container, içeriği ortalamak ve kırmızı arka plan uygulamak için */
+.assent {
+	background-color: #CE2829; /* Kırmızı arka plan */
+	display: flex; /* Flexbox kullanarak düzenleme */
+	flex-direction: column; /* İçerikleri dikey olarak sıralama */
+	justify-content: center; /* Dikeyde ortalama */
+	align-items: center; /* Yatayda ortalama */
+  }
+  /* Sarı renkte metin için stil */
+.yellowText {
+	font-family: Satisfy; /* Satisfy fontu */
+	font-size: 2rem;  /* Font büyüklüğü */
+	color: #FDC913; /* Sarı renk */
+  }
