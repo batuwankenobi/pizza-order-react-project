335b3c31ca34e5e5b62002f8a4f4efcb41537d08
Batuhan Bartu Bebek <batuhanbartuu@gmail.com>
2024-12-31 20:57:56 +0300

    bug fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export function Header() {
+const Main = () => {
   return (
-    <div className="flex-col">
-      <div className=" flex-col text-center barlow">
-        <NavLink
-          to="/"
-          className={(isActive) => "header" + (!isActive ? "header" : "")}
-        >
-          <h1>Teknolojik Yemekler</h1>
-        </NavLink>
+    <div className="hero-section">
+      <div className="mt-m">
+        <p>fırsatı kaçırma</p>
+        <h2>
+          KOD ACIKTIRIR
+          <br />
+          PİZZA, DOYURUR
+        </h2>
+        <div className="flex-center mt-s">
+          <button>
+            <NavLink to="/siparisFormu">ACIKTIM</NavLink>
+          </button>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default Main;
